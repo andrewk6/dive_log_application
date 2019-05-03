a9655302993c28d59d52921753b1508089cb448e
Andrew Knox <andrew.knox112@gmail.com>
2019-05-02 20:10:09 -0400

    Improved Navy Dive Tables

--- src/Navy_Table.java
+++ src/Navy_Table.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Set;
 
 public class Navy_Table{
 	
@@ -24,11 +25,22 @@ public class Navy_Table{
 	
 	public static final char getLetter(int depth, int mins) {
 		char letter = 'A';
-//		switch(depth) {
-//		case 10: switch(mins)
-//		}
 		
+		switch(depth) {
+		case 10: letter = depth_table_check(depth_10ft, mins);
+		}
 		return letter;
+	}
+	
+	private static final char depth_table_check(HashMap<Integer, Character> dep, int mins) {
+		char let = ' ';
+		Set<Integer> keys = dep.keySet();
+		for(Integer i : keys) {
+			if(!(i < mins)) {
+				return dep.get(i);
+			}
+		}
+		return let;
 	}
 	
 	public static final int getNoDecompLim(int depth) {
@@ -164,5 +176,157 @@ public class Navy_Table{
 		depth_40ft.put(84, 'I');
 		depth_40ft.put(95, 'J');
 		depth_40ft.put(108, 'K');
+		depth_40ft.put(121, 'L');
+		depth_40ft.put(135, 'M');
+		depth_40ft.put(151, 'N');
+		depth_40ft.put(163, 'O');
+	}
+
+	public static final void fillTable45ft() {
+		depth_45ft.put(11, 'A');
+		depth_45ft.put(17, 'B');
+		depth_45ft.put(24, 'C');
+		depth_45ft.put(31, 'D');
+		depth_45ft.put(39, 'E');
+		depth_45ft.put(46, 'F');
+		depth_45ft.put(55, 'G');
+		depth_45ft.put(63, 'H');
+		depth_45ft.put(72, 'I');
+		depth_45ft.put(82, 'J');
+		depth_45ft.put(92, 'K');
+		depth_45ft.put(102, 'L');
+		depth_45ft.put(114, 'M');
+		depth_45ft.put(125, 'N');
+	}
+
+	public static final void fillTable50ft() {
+		depth_50ft.put(9, 'A');
+		depth_50ft.put(15, 'B');
+		depth_50ft.put(21, 'C');
+		depth_50ft.put(28, 'D');
+		depth_50ft.put(34, 'E');
+		depth_50ft.put(41, 'F');
+		depth_50ft.put(48, 'G');
+		depth_50ft.put(56, 'H');
+		depth_50ft.put(63, 'I');
+		depth_50ft.put(71, 'J');
+		depth_50ft.put(80, 'K');
+		depth_50ft.put(89, 'L');
+		depth_50ft.put(92, 'M');
+	}
+
+	public static final void fillTable55ft() {
+		depth_55ft.put(8, 'A');
+		depth_55ft.put(14, 'B');
+		depth_55ft.put(19, 'C');
+		depth_55ft.put(25, 'D');
+		depth_55ft.put(31, 'E');
+		depth_55ft.put(37, 'F');
+		depth_55ft.put(43, 'G');
+		depth_55ft.put(50, 'H');
+		depth_55ft.put(56, 'I');
+		depth_55ft.put(63, 'J');
+		depth_55ft.put(71, 'K');
+		depth_55ft.put(74, 'L');
+	}
+
+	public static final void fillTable60ft() {
+		depth_60ft.put(7, 'A');
+		depth_60ft.put(12, 'B');
+		depth_60ft.put(17, 'C');
+		depth_60ft.put(22, 'D');
+		depth_60ft.put(28, 'E');
+		depth_60ft.put(33, 'F');
+		depth_60ft.put(39, 'G');
+		depth_60ft.put(45, 'H');
+		depth_60ft.put(51, 'I');
+		depth_60ft.put(57, 'J');
+		depth_60ft.put(60, 'K');
+	}
+
+	public static final void fillTable70ft() {
+		depth_70ft.put(6, 'A');
+		depth_70ft.put(10, 'B');
+		depth_70ft.put(14, 'C');
+		depth_70ft.put(19, 'D');
+		depth_70ft.put(23, 'E');
+		depth_70ft.put(28, 'F');
+		depth_70ft.put(32, 'G');
+		depth_70ft.put(37, 'H');
+		depth_70ft.put(42, 'I');
+		depth_70ft.put(47, 'J');
+		depth_70ft.put(48, 'K');
+	}
+
+	public static final void fillTable80ft() {
+		depth_80ft.put(5, 'A');
+		depth_80ft.put(9, 'B');
+		depth_80ft.put(12, 'C');
+		depth_80ft.put(16, 'D');
+		depth_80ft.put(20, 'E');
+		depth_80ft.put(24, 'F');
+		depth_80ft.put(28, 'G');
+		depth_80ft.put(32, 'H');
+		depth_80ft.put(36, 'I');
+		depth_80ft.put(39, 'J');
+	}
+
+	public static final void fillTable90ft() {
+		depth_90ft.put(4, 'A');
+		depth_90ft.put(7, 'B');
+		depth_90ft.put(11, 'C');
+		depth_90ft.put(14, 'D');
+		depth_90ft.put(17, 'E');
+		depth_90ft.put(21, 'F');
+		depth_90ft.put(24, 'G');
+		depth_90ft.put(28, 'H');
+		depth_90ft.put(30, 'I');
+	}
+
+	public static final void fillTable100ft() {
+		depth_100ft.put(4, 'A');
+		depth_100ft.put(6, 'B');
+		depth_100ft.put(9, 'C');
+		depth_100ft.put(12, 'D');
+		depth_100ft.put(15, 'E');
+		depth_100ft.put(18, 'F');
+		depth_100ft.put(21, 'G');
+		depth_100ft.put(25, 'H');
+	}
+
+	public static final void fillTable110() {
+		depth_110ft.put(3, 'A');
+		depth_110ft.put(6, 'B');
+		depth_110ft.put(8, 'C');
+		depth_110ft.put(11, 'D');
+		depth_110ft.put(14, 'E');
+		depth_110ft.put(16, 'F');
+		depth_110ft.put(19, 'G');
+		depth_110ft.put(20, 'H');
+	}
+
+	public static final void fillTable120() {
+		depth_120ft.put(3, 'A');
+		depth_120ft.put(5, 'B');
+		depth_120ft.put(7, 'C');
+		depth_120ft.put(10, 'D');
+		depth_120ft.put(12, 'E');
+		depth_120ft.put(15, 'F');
+	}
+
+	public static final void fillTable130() {
+		depth_130ft.put(2, 'A');
+		depth_130ft.put(4, 'B');
+		depth_130ft.put(6, 'C');
+		depth_130ft.put(9, 'D');
+		depth_130ft.put(10, 'E');
+	}
+	
+	public static final void fillTable140() {
+		depth_140ft.put(2, 'A');
+		depth_140ft.put(4, 'B');
+		depth_140ft.put(6, 'C');
+		depth_140ft.put(8, 'D');
+		depth_140ft.put(10, 'E');
 	}
 }
